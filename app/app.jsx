aee2a7c2a57b502490cc516fb7e949ec5f4f1c3a
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-09-04 19:55:08 +0100

    rendering to dom

---
+++ app/app.jsx
@@ -0,0 +1,5 @@
+var React = require('react')
+var ReactDom = require('react-dom')
+var StoreLocator = require('./components/StoreLocator')
+
+ReactDom.render(<StoreLocator/>, document.getElementById('app'))
