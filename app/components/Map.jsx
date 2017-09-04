cb529004e17c7c27dede0e3779a5d5d2539657ce
Linton Mackereth <linton@Lintons-MBP.home>
2017-09-04 20:42:42 +0100

    google maps api and gmaps linked

---
+++ app/components/Map.jsx
@@ -0,0 +1,26 @@
+var React = require('react')
+
+var Map = React.createClass({
+  componentDidMount: function () {
+    this.componentDidUpdate()
+  },
+  componentDidUpdate: function () {
+    var map = new GMaps({
+      el: '#map',
+      lat: this.props.lat,
+      lng: this.props.lng
+    })
+    map.addMarker({
+      lat: this.props.lat,
+      lng: this.props.lng,
+      label: 'M'
+    })
+  },
+  render: function () {
+    return (
+      <div id='map' />
+    )
+  }
+})
+
+module.exports = Map
--- app/components/StoreLocator.jsx
+++ app/components/StoreLocator.jsx
@@ -1,15 +1,18 @@
 var React = require('react')
+var Map = require('./Map')
 
 var StoreLocator = React.createClass({
   render: function () {
     return (
-      <div className="store-locator">
-        <div className="location-column">
-          <div className="title">
+      <div className='store-locator'>
+        <div className='location-column'>
+          <div className='title'>
             Store Locator
           </div>
         </div>
-        <div className="map"></div>
+        <div className='map'>
+          <Map lat='53.4719986' lng='-2.2414979' />
+        </div>
       </div>
     )
   }
